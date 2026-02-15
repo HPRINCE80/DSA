95799084db77be731b8dba3e130b2153372a1538
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-02-15 19:16:11 +0530

    Insert

--- Binary_Search/problem04.cpp
+++ Binary_Search/problem04.cpp
@@ -1,55 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-class FloorCeilFinder {
-public:
-    // Function to find the floor of x
-    int findFloor(int arr[], int n, int x) {
-        int low = 0, high = n - 1;
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] <= x) {
-                ans = arr[mid];     // Potential floor
-                low = mid + 1;      // Search right side
-            } else {
-                high = mid - 1;     // Search left side
-            }
+int SearchInsert(vector<int>& nums, int target){
+    int left=0;
+    int right = nums.size() -1;
+    int ans = nums.size();
+    while(left<=right){
+        int mid =  (right+left)/2;
+        if(nums[mid]>=target){
+            ans = mid;
+            right = mid -1;
+            
+        }
+        
+        
+        else{
+            left = mid +1;
+        }
         }
         return ans;
-    }
-
-    // Function to find the ceiling of x
-    int findCeil(int arr[], int n, int x) {
-        int low = 0, high = n - 1;
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] >= x) {
-                ans = arr[mid];     // Potential ceil
-                high = mid - 1;     // Search left side
-            } else {
-                low = mid + 1;      // Search right side
-            }
-        }
-        return ans;
-    }
-
-    // Function to return both floor and ceil
-    pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
-        int f = findFloor(arr, n, x);
-        int c = findCeil(arr, n, x);
-        return make_pair(f, c);
-    }
-};
-
-int main() {
-    int arr[] = {3, 4, 4, 7, 8, 10};
-    int n = 6, x = 5;
-    FloorCeilFinder finder;
-    pair<int, int> ans = finder.getFloorAndCeil(arr, n, x);
-    cout << "The floor and ceil are: " << ans.first << " " << ans.second << endl;
+}
+int main(){
+    vector<int> nums = {1, 3, 5, 6};
+    int target = 2;
+    cout<<"Target Value is inserted and Present at index number-> "<<SearchInsert(nums,target);
+    
     return 0;
+
 }
