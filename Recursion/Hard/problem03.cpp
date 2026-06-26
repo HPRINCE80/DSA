f6ca36e270cd4e6c93982ff35ee617d32ce91211
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-06-26 10:20:47 +0530

    LL

--- Recursion/Hard/problem03.cpp
+++ Recursion/Hard/problem03.cpp
@@ -1,7 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool isSafe(int vertex, int color, vector<int>& colors, vector<vector<int>>& adj){
+    for(int neighbor : adj[vertex]){
+        if(colors[neighbor] == color) return false;
+    }
+    return true;
+}
+bool solve(int vertex, vector<int>& colors, vector<vector<int>>& adj, int M, int N){
+    if(vertex == N) return true;
+    for(int color = 1; color <= M; color++){
+    if(isSafe(vertex, color, colors, adj)){
+        colors[vertex] = color;
+        
+        if(solve(vertex+1, colors, adj, M, N)) return true;
+        
+        colors[vertex] = 0;  // backtrack
+    }
+}
+return false;
+}
 
 int main(){
-
+    int N = 4, M = 3;
+    vector<vector<int>> adj(N);
+    
+    // edges add karo: 0-1, 1-2
+    adj[0].push_back(1);
+    adj[1].push_back(2);
+    adj[1].push_back(3);
+    adj[2].push_back(4);
+    
+    vector<int> colors(N, 0);
+    cout<<boolalpha;
+    cout << solve(0, colors, adj, M, N);
+    
     return 0;
+
 }
--- Recursion/Hard/problem05.cpp
+++ Recursion/Hard/problem05.cpp
@@ -1,43 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool isPalindrome(string s)
-{
-    string rev = s;
-    reverse(rev.begin(), rev.end());
-    return s == rev;
-}
-void palindrome(string s, int index, vector<string> current, vector<vector<string>> &result)
-{
-    if (index == s.size())
-    {
-        result.push_back(current);
-        return;
-    }
 
-    for (int i = index; i < s.size(); i++)
-    {
-        string sub = s.substr(index, i - index + 1);
-        if (isPalindrome(sub))
-        {
-            current.push_back(sub);
-            palindrome(s, i + 1, current, result);
-            current.pop_back();
+    // Function to evaluate the expression
+    void dfs(string& num, int target, int start, long long current_value, long long last_operand, string expression, vector<string>& result) {
+        // Base case: If we've reached the end of the string
+        if (start == num.size()) {
+            // If the expression evaluates to the target, add it to result
+            if (current_value == target)  
+                result.push_back(expression);
+            return;
+        }
+        
+        // Loop through all substrings starting from 'start' index
+        for (int i = start; i < num.size(); i++) {
+            // Skip leading zeros in numbers
+            if (i > start && num[start] == '0') return;
+            // Get the current number
+            string current_num = num.substr(start, i - start + 1); 
+            long long current_num_val = stoll(current_num);
+            
+            // If we are at the first number, just start the expression
+            if (start == 0) {
+                dfs(num, target, i + 1, current_num_val, current_num_val, current_num, result);
+            } else {
+                // Add the current number with '+'
+                dfs(num, target, i + 1, current_value + current_num_val, current_num_val, expression + "+" + current_num, result);
+                
+                // Add the current number with '-'
+                dfs(num, target, i + 1, current_value - current_num_val, -current_num_val, expression + "-" + current_num, result);
+                
+                // Add the current number with '*'
+                dfs(num, target, i + 1, current_value - last_operand + last_operand * current_num_val, last_operand * current_num_val, expression + "*" + current_num, result);
+            }
         }
     }
-}
-int main()
-{
-    string s = "aabaa";
-    vector<vector<string>> result;
-    palindrome(s, 0, {}, result);
-    for (vector<string> subset : result)
-    {
-        cout << "{";
-        for (string word : subset)
-        {
-            cout << word << " ";
-        }
+    // To store the valid expressions
+    vector<string> addOperators(string num, int target) {
+        vector<string> result;  
+        // Start DFS with empty expression
+        dfs(num, target, 0, 0, 0, "", result);  
+        return result;
+    }
 
+
+int main() {
+    string num = "123";  
+    int target = 6;      
+           
+    
+    // Get all valid expressions
+    vector<string> result = addOperators(num, target);
+    
+    // Print the results
+    for (const string& expr : result) {
+        cout << expr << " "; 
     }
     return 0;
 }
