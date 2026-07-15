36742767ae1e3a76ad9dff74314a0b81eaac1e15
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-07-15 21:28:33 +0530

    LL

--- Revise DSA/Recursion/atoi.cpp
+++ Revise DSA/Recursion/atoi.cpp
@@ -4,4 +4,26 @@ int main(){
     
     return 0;
 
+}#include <bits/stdc++.h>
+using namespace std;
+void gcd(int a, int b)
+{
+    for (int i = 1; i <= min(a, b); i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            cout << i << " ";
+        }
+    }
+}
+ 
+void optimals(int a, int b){
+    int res =0;
+    
+}
+int main()
+{
+    int a = 4, b = 6;
+    gcd(a, b);
+    return 0;
 }
