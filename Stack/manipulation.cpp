a3ad13af18426be9d9866bb567c3a11dcb9d6aa0
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-08-02 20:15:32 +0530

    LL

---
+++ Stack/manipulation.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int countword(vector<string>& v){
+    stack<string> s;
+
+    for(string word : v){
+        if(!s.empty() && s.top() == word){
+            s.pop();
+        }
+        else{
+            s.push(word);
+        }
+        }
+        return s.size();
+    }
+
+int main(){
+    vector<string> arr = {"hello", "world", "world", "hello", "hello"};
+    cout<<"word left:"<<countword(arr)<<endl;
+    
+    return 0;
+}
