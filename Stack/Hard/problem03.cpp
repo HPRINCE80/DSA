23b4df8a716dbc791504c83e657bb2e8c28a9a4d
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-08-11 17:29:22 +0530

    trap rain water

---
+++ Stack/Hard/problem03.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> nextGreaterElement(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> result(n);
+    stack<int> st;
+    
+    for (int i = n - 1; i >= 0; i--) {
+        while(!st.empty() && st.top()>= arr[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            result[i] = -1;
+
+        } else{
+            result[i] = st.top();
+
+        }
+        st.push(arr[i]);
+
+        
+        // 2. agar stack khali hai, result[i] = -1, warna result[i] = stack.top()
+        // 3. arr[i] ko push kar do
+    }
+    return result;
+}
+
+
+int main() {
+    vector<int> arr = {4, 8, 5, 2, 25};
+    vector<int> result = nextGreaterElement(arr);
+    
+    for (int x : result) cout << x << " ";
+    // Expected: 5 25 25 -1
+    
+    return 0;
+}
