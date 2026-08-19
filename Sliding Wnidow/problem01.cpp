ee812bf84c25e55c8ecc5230bdcf0856e9fa14ca
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-08-19 16:18:49 +0530

    maxone

--- Sliding Wnidow/problem01.cpp
+++ Sliding Wnidow/problem01.cpp
@@ -1,30 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int lengthOfLongestSubstring(string s) {
-    unordered_map<char, int> charIndex;  // character -> last seen index
+int logeststring(string& s){
+    unordered_map<char, int> charIndex;
     int left = 0;
-    int maxLen = 0;
+    int maxlen = 0;
 
-    for (int right = 0; right < s.size(); right++) {
+    for(int right = 0;right<s.size();right++){
         char ch = s[right];
 
-        // Agar character pehle dekha ja chuka hai AND wo current window ke andar hai
-        if (charIndex.find(ch) != charIndex.end() && charIndex[ch] >= left) {
-            left = charIndex[ch] + 1;   // window shrink karo
-        }
-
-        charIndex[ch] = right;   // update last seen index
-        maxLen = max(maxLen, right - left + 1);
+        if(charIndex.find(ch) != charIndex.end() && charIndex[ch] >= left){
+            left = charIndex[ch] + 1;
+                }
+                charIndex[ch] = right;
+                maxlen =max(maxlen, right - left +1);
     }
+    return maxlen;
 
-    return maxLen;
-}  
-
-int main()
-{
+}
+int main() {
     string s = "cadbzabcd";
-    int result = lengthOfLongestSubstring(s);
-    cout << result << endl;
+    int result =  logeststring(s);
+    cout<<result << endl;
+
+
     return 0;
 }
