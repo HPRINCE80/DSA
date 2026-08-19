3d331462424b76eb567443e4d909c02f6166f432
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-08-19 14:11:08 +0530

    add

--- Sliding Wnidow/problem01.cpp
+++ Sliding Wnidow/problem01.cpp
@@ -1,37 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int longestNonRepeating(string &s)
-{
-    int n = s.size();
-    int HashLen = 256;
+int lengthOfLongestSubstring(string s) {
+    unordered_map<char, int> charIndex;  // character -> last seen index
+    int left = 0;
+    int maxLen = 0;
 
-    int hash[HashLen];
+    for (int right = 0; right < s.size(); right++) {
+        char ch = s[right];
 
-    for (int i = 0; i < HashLen; i++)
-    {
-        hash[i] = -1;
-    }
-    int l = 0, r = 0, maxLen = 0;
-    while (r < n)
-    {
-        if (hash[s[r]] != -1)
-        {
-            l = max(hash[s[r]] + 1, 1);
+        // Agar character pehle dekha ja chuka hai AND wo current window ke andar hai
+        if (charIndex.find(ch) != charIndex.end() && charIndex[ch] >= left) {
+            left = charIndex[ch] + 1;   // window shrink karo
         }
-        int len = r - 1 + 1;
 
-        maxLen = max(len, maxLen);
-
-        hash[s[r]] = r;
-        r++;
+        charIndex[ch] = right;   // update last seen index
+        maxLen = max(maxLen, right - left + 1);
     }
+
     return maxLen;
-}
+}  
+
 int main()
 {
     string s = "cadbzabcd";
-    int result = longestNonRepeating(s);
+    int result = lengthOfLongestSubstring(s);
     cout << result << endl;
     return 0;
 }
