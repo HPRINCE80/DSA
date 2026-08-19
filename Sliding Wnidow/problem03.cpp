dc4dbb34ab6c076acddcb099df7f3d278c79761d
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-08-19 16:41:02 +0530

    fruits

---
+++ Sliding Wnidow/problem03.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fruites(vector<int>& nums){
+    int maxfruite = 0;
+    int n = nums.size();
+    for(int i =0;i<n;i++){
+        unordered_map<int , int> basket;
+        int currentCount = 0;
+        for(int j=0;j<n;++j){
+            basket[nums[j]]++;
+
+
+            if(basket.size() > 2){
+                break;
+            }
+            currentCount++;
+        }
+        maxfruite = max(maxfruite, currentCount);
+    }
+    return maxfruite;
+}
+int main() {
+
+    vector<int> fruits = {1, 2, 1};
+    cout <<fruites(fruits) << endl;
+    return 0;
+}
