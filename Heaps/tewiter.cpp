614b2aad9fe94dd1c487141beab54016e21e3fbf
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-09-17 22:45:21 +0530

    dF

--- Heaps/tewiter.cpp
+++ Heaps/tewiter.cpp
@@ -1,94 +1,90 @@
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// Class representing the Twitter system
-class Twitter {
-private:
-    // Stores the tweets of each user as a vector of (timestamp, tweetId)
-    unordered_map<int, vector<pair<int, int>>> tweets;
-    // Stores the set of followees for each user
-    unordered_map<int, unordered_set<int>> following;
-    // Global time counter to sort tweets
-    int time;
 
-public:
-    // Constructor to initialize time counter
-    Twitter() {
-        time = 0;
-    }
+    // Function to calculate least number of units of time to finish all tasks with cooldown
+    int leastInterval(vector<char>& tasks, int n) {
 
-    // Function to post a tweet
-    void postTweet(int userId, int tweetId) {
-        // Add the tweet with current timestamp to user's tweet list
-        tweets[userId].push_back({time++, tweetId});
-    }
-
-    // Function to get the 10 most recent tweets in news feed
-    vector<int> getNewsFeed(int userId) {
-        // Min-heap to store tweets based on timestamp
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-
-        // Insert user's own tweets
-        for (auto& t : tweets[userId]) {
-            pq.push(t);
-            // Keep heap size max 10
-            if (pq.size() > 10)
-                pq.pop();
+        // Step 1: Count frequency of each task
+        unordered_map<char, int> freq;
+        for (char task : tasks) {
+            freq[task]++;
         }
 
-        // Insert tweets from users being followed
-        for (int followee : following[userId]) {
-            for (auto& t : tweets[followee]) {
-                pq.push(t);
-                if (pq.size() > 10)
-                    pq.pop();
+        // Step 2: Use max heap to always pick task with highest frequency
+        priority_queue<int> maxHeap;
+        for (auto& entry : freq) {
+            maxHeap.push(entry.second);
+        }
+
+        // Total time required
+        int time = 0;
+
+        // Step 3: Process tasks in cycles of size (n + 1)
+        while (!maxHeap.empty()) {
+
+            // Store tasks executed in this cycle
+            vector<int> temp;
+
+            // Set number of tasks allowed in this cycle
+            int cycle = n + 1;
+
+            // Counter to track how many tasks we run in this cycle
+            int i = 0;
+
+            // Keep running tasks until cycle ends or heap gets empty
+            while (i < cycle && !maxHeap.empty()) {
+
+                // Get the most frequent remaining task
+                int cnt = maxHeap.top();
+                maxHeap.pop();
+
+                // Reduce its count as we’re running it once
+                cnt--;
+
+                // If the task still has pending frequency, store it for later
+                if (cnt > 0) {
+                    temp.push_back(cnt);
+                }
+
+                // Count 1 unit of time for this task
+                time++;
+
+                // Move to next slot in the cycle
+                i++;
             }
+
+            // Step 4: Push leftover tasks of this cycle back into the heap
+            for (int val : temp) {
+                maxHeap.push(val);
+            }
+
+            // Step 5: If heap is empty, no need to count idle time
+            if (maxHeap.empty()) break;
+
+            // Step 6: Add idle time to complete the cycle (if any slots were left)
+            time += (cycle - i);
         }
 
-        // Extract tweets from heap in reverse order
-        vector<int> res;
-        while (!pq.empty()) {
-            res.push_back(pq.top().second);
-            pq.pop();
-        }
-        reverse(res.begin(), res.end());
-        return res;
+        // Return the total time needed
+        return time;
     }
 
-    // Function to follow another user
-    void follow(int followerId, int followeeId) {
-        following[followerId].insert(followeeId);
-    }
-
-    // Function to unfollow another user
-    void unfollow(int followerId, int followeeId) {
-        following[followerId].erase(followeeId);
-    }
-};
 
 // Driver code
 int main() {
-    Twitter twitter;
+    
 
-    twitter.postTweet(1, 2);
-    twitter.postTweet(2, 6);
+    // Input task list
+    vector<char> tasks = {'A','A','A','B','B','B'};
 
-    vector<int> res1 = twitter.getNewsFeed(1);
-    for (int i : res1) cout << i << " ";
-    cout << endl;
+    // Cooldown period
+    int n = 2;
 
-    twitter.follow(1, 2);
-
-    vector<int> res2 = twitter.getNewsFeed(1);
-    for (int i : res2) cout << i << " ";
-    cout << endl;
-
-    twitter.unfollow(1, 2);
-    twitter.postTweet(1, 7);
-
-    vector<int> res3 = twitter.getNewsFeed(1);
-    for (int i : res3) cout << i << " ";
-    cout << endl;
+    // Output the least interval required to finish all tasks
+    cout << leastInterval(tasks, n) << endl;
 
     return 0;
 }
